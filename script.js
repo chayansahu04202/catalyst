b0ac4859ff5e3168e27ec9c9728e2e2b395ce192
chayan <107623938+chayansahu04202@users.noreply.github.com>
2022-07-09 23:59:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["Performance", "Influencer", "Marketing"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["Performance", "Influencer", "Marketing"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+// owl carousel script
+$('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 2,
+            nav: false
+        },
+        1000:{
+            items: 3,
+            nav: false
+        }
+    }
+});
